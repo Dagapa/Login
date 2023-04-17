9ecdce54217c2403790989399ffbbbf1149e0816
PzzaBoy <jbustosmoya@gmail.com>
2023-04-17 14:31:51 -0500

    on1

--- Client/src/utils/get.js
+++ Client/src/utils/get.js
@@ -1,10 +1,8 @@
 export async function user() {
   try {
-<<<<<<< HEAD
-    const response = await fetch("https://login-production-c7e0.up.railway.app/users");
-=======
+
     const response = await fetch("login-production-272a.up.railway.app/users");
->>>>>>> 2fff7c9cb8871e816e3122cc3a76d92962386625
+
     const data = await response.json();
     return data;
   } catch (error) {
@@ -14,11 +12,9 @@ export async function user() {
 
 export async function students() {
   try {
-<<<<<<< HEAD
-    const response = await fetch("https://login-production-c7e0.up.railway.app/students");
-=======
+
     const response = await fetch("login-production-272a.up.railway.app/students");
->>>>>>> 2fff7c9cb8871e816e3122cc3a76d92962386625
+
     const data = await response.json();
     return data;
   } catch (error) {
