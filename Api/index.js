123378b4a794764c2da9e77743c6ad5564a5bc25
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-04-18 01:08:21 -0500

    change a api

--- Api/index.js
+++ Api/index.js
@@ -2,79 +2,48 @@ const sql = require("mssql");
 const express = require("express");
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const config = {
+const dbConfig = {
   user: "Adminuser",
   password: "Pala9910*",
   server: "sr-sqlserverline03.database.windows.net",
   database: "lineasql",
 };
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
+// Función para obtener los resultados de una consulta a la base de datos
+const getQueryResult = async (query) => {
+  try {
+    const pool = await sql.connect(dbConfig);
+    const result = await pool.request().query(query);
+    await sql.close();
+    return result.recordset;
+  } catch (err) {
+    console.error("Error de consulta:", err);
+    throw new Error("Error al consultar la base de datos");
+  }
+};
+
+// Endpoint para obtener todos los usuarios
+app.get("/users", async (req, res) => {
+  try {
+    const queryResult = await getQueryResult("SELECT * FROM Users");
+    res.send(queryResult);
+  } catch (err) {
+    res.status(500).send("Error al obtener los usuarios");
+  }
 });
 
-app.get("/users", (req, res) => {
-  const pool = new sql.ConnectionPool(config);
-
-  pool.connect((err) => {
-    if (err) {
-      console.error("Error de conexión:", err.message);
-      return;
-    }
-
-    const request = pool.request();
-
-    request.query("SELECT * FROM Users", (err, result) => {
-      if (err) {
-        console.error("Error de consulta:", err);
-        return;
-      }
-      console.log(result.recordset);
-
-      // Cierra la conexión después de obtener los resultados
-      pool.close();
-
-      // Envía los resultados como respuesta
-      res.send(result.recordset);
-    });
-  });
+// Endpoint para obtener todos los alumnos
+app.get("/students", async (req, res) => {
+  try {
+    const queryResult = await getQueryResult("SELECT * FROM Alumnos");
+    res.send(queryResult);
+  } catch (err) {
+    res.status(500).send("Error al obtener los alumnos");
+  }
 });
 
-app.get("/students", (req, res) => {
-  const pool = new sql.ConnectionPool(config);
-
-  pool.connect((err) => {
-    if (err) {
-      console.error("Error de conexión:", err.message);
-      return;
-    }
-
-    const request = pool.request();
-
-    request.query("SELECT * FROM Alumnos", (err, result) => {
-      if (err) {
-        console.error("Error de consulta:", err);
-        return;
-      }
-      console.log(result.recordset);
-
-      // Cierra la conexión después de obtener los resultados
-      pool.close();
-
-      // Envía los resultados como respuesta
-      res.send(result.recordset);
-    });
-  });
-});
-
-app.listen(3001, () => {
-  console.log("Servidor iniciado en el puerto 3000");
+app.listen(PORT, () => {
+  console.log("Servidor iniciado en el puerto " + PORT);
 });
